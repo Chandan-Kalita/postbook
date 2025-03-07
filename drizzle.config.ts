4646ae4d8b05663dc6786bad071cc589197f9fc0
Chandan Kalita <ck3196686@gmail.com>
2025-03-07 22:54:55 +0530

    fix

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
   out: './apps/user_service/generated/drizzle',
-  schema: '.apps/user_service/src/db/schema.ts',
+  schema: './apps/user_service/src/db/schema.ts',
   dialect: 'postgresql',
   dbCredentials: {
     url: process.env.DATABASE_URL!,
